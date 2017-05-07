30e89b66955c698653bb17a59e1b57731de208c1
Daniel Bratulic <daniel.bratulic@microblink.com>
2017-05-07 11:01:20 +0200

    Expanded the Sentence object with article and question tag

--- implementations/baseline/sentence.py
+++ implementations/baseline/sentence.py
@@ -3,7 +3,9 @@ from interfaces.i_sentence import ISentence
 
 
 class Sentence(ISentence):
-    def __init__(self, wordList):
+    def __init__(self, wordList, article_ID=None, question_ID=None):
+        self.article_ID = article_ID
+        self.question_ID= question_ID
         if wordList is None:
             self.wordList = list()
         else:
@@ -14,6 +16,12 @@ class Sentence(ISentence):
 
     def getWordByAddress(self, address):
         return self.wordList[address - 1]
+
+    def get_article_ID(self):
+        return self.article_ID
+
+    def get_question_ID(self):
+        return self.question_ID
 
     def __str__(self):
         return " ".join([word.wordText for word in self.wordList])
