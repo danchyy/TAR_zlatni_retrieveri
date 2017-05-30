2d5849491325405e6f1db8c5d207c1bf30c25565
Daniel Bratulic <daniel.bratulic@microblink.com>
2017-05-30 17:51:07 +0200

    Script for creating training and test data indexes

--- temp_script.py
+++ temp_script.py
@@ -8,7 +8,7 @@ for line in lines:
 		print(line)"""
 
 # !/usr/bin/ python2
-import pickle
+"""
 
 objects = pickle.load(open("pickles/temp_pickle2.p", "rb"))
 
@@ -24,7 +24,6 @@ for key in objects:
     count += 1
     if count > 20:
         break
-"""
 for currSent in objects:
 	sentence = currSent.__str__()
 	article_id = currSent.get_article_ID()
@@ -43,3 +42,42 @@ all_file.close()
 tagged_file.close()
 """
 # print("aaa" == None)
+import cPickle as pickle
+import numpy as np
+
+sentences = pickle.load(open("pickles/labeled_sentences.pickle", "rb"))
+
+print sentences[0]
+
+positive_counter = {}
+negative_counter = {}
+
+for sentence in sentences:
+    for question_id, label in sentence[2]:
+        if label=='-1':
+            count = negative_counter.get(question_id, 0)+1
+            negative_counter[question_id] = count
+        elif label=='1':
+            count = positive_counter.get(question_id, 0)+1
+            positive_counter[question_id] = count
+
+
+
+question_ids = np.array(positive_counter.keys())
+
+train_ids = question_ids[np.random.choice(664, 531)]
+print len(train_ids)
+test_ids = np.array(list(set(question_ids)-set(train_ids)))
+"""test_ids = np.array([])
+for key in positive_counter:
+    if key not in train_ids:
+        np.append(test_ids, key)"""
+
+print train_ids
+print test_ids
+
+print 'a'
+print len(set(train_ids)&set(test_ids))
+
+np.save(open("data/train_indexes.npy", "wb"), train_ids)
+np.save(open("data/test_indexes.npy", "wb"), test_ids)
